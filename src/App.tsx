0e7ee58b8dffd7d09e840dd30068ec4a60ba61ad
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-04-15 10:51:30 +0900

    Create MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
+import pinyin from "pinyin";
 
 function App(): JSX.Element {
-  return <>Hello World!</>;
+  const [inputChineseText, setInputChineseText] = useState<string>("");
+  return (
+    <>
+      <input
+        type="text"
+        value={inputChineseText}
+        onChange={(e) => {
+          setInputChineseText(e.target.value);
+        }}
+      />
+      {Array.from(inputChineseText).map((chineseChar, i) => (
+        <Fragment key={i}>
+          <ruby style={{ margin: "0.3rem" }}>
+            {chineseChar} <rp>(</rp>
+            <rt>
+              {pinyin(chineseChar, {
+                heteronym: true,
+                style: pinyin.STYLE_TONE,
+              }).join("")}
+            </rt>
+            <rp>)</rp>
+          </ruby>
+        </Fragment>
+      ))}
+    </>
+  );
 }
 
 export default App;
