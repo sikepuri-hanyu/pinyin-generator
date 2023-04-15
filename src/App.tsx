7fc33602d14ccd7c8055fd27772101dc5796b10a
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-04-15 12:00:43 +0900

    Add lang prop

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App(): JSX.Element {
         multiline
         minRows={5}
         fullWidth
+        lang="zh-cmn-Hans"
         value={inputChineseText}
         onChange={(e) => {
           setInputChineseText(e.target.value);
@@ -19,7 +20,7 @@ function App(): JSX.Element {
       <Paper style={{ minHeight: "10rem", padding: 10 }}>
         {Array.from(inputChineseText).map((chineseChar, i) => (
           <Fragment key={i}>
-            <ruby style={{ margin: "0.3rem" }}>
+            <ruby style={{ margin: "0.3rem" }} lang="zh-cmn-Hans">
               {chineseChar} <rp>(</rp>
               <rt>
                 {pinyin(chineseChar, {
